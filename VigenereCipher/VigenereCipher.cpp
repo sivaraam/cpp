7c7224ab960735b81d85dcb1472f5fc10454e936
Kaartic Sivaraam <kaarticsivaraam91196@gmail.com>
2017-07-16 11:23:04 +0530

    Revert "VigenereCipher: add asserts to ensure the index is not negative"
    This reverts commit 48c25072e60beb8d555c6422330f0bca75ff9433.
    
    The expression inside the assert is not a compile time constant and
    hence static_assert cannot be used.

--- VigenereCipher/VigenereCipher.cpp
+++ VigenereCipher/VigenereCipher.cpp
@@ -17,9 +17,6 @@ namespace VigenereCipher {
 		}
 
 		size_t original_index = toupper(index) - 'A';
-		// Index should always be greater than zero
-		static_assert(original_index >= 0);
-
 		return word[original_index];
 	}
 	// ------------- END : VigenereRow ----------------
@@ -42,9 +39,6 @@ namespace VigenereCipher {
 			throw std::invalid_argument { "Index should be an alphabet" };
 
 		size_t original_index = toupper(index) - 'A';
-		// Index should always be greater than zero
-		static_assert(original_index >= 0);
-
 		return rows[original_index];
 	}
 	// --------------- END : VigenereTable -------------
