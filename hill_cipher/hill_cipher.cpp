cada3d0b03f4f825ecb7461d4b504df9ba46e9a3
Kaartic Sivaraam <kaarticsivaraam91196@gmail.com>
2017-07-26 21:54:50 +0530

    hill_cipher: add partial implementation

---
+++ hill_cipher/hill_cipher.cpp
@@ -0,0 +1,109 @@
+#include "hill_cipher.h"
+#include <vector>
+#include <cmath>
+#include <exception>
+
+namespace hill_cipher {
+
+	istream& operator>> (istream& is, cipher_matrix& cmatrix) {
+		for (int i=0; i<cmatrix.degree; i++) {
+			for (int j=0; j<cmatrix.degree; j++) {
+				is>>cmatrix.matrix[i][j];
+				/* TODO	
+				if(is::fail) {
+					throw new std::exception("Could not read the cipher matrix");
+				}*/
+			}
+
+		}
+	}
+
+	static bool is_valid(char ch) {
+		if(isalpha(ch) && isupper(ch))
+			return true;
+
+		return false;
+	}
+
+	// converts uppercase alphabet to a value iin range [0, 26)
+	static int value(char ch) {
+		if(!is_valid(ch))
+			throw new invalid_argument_exception("Not a valid character\n"
+								  "Only uppercase alphabets are valid");
+		return (ch - 'A');
+	}
+
+	static bool is_valid(int val) {
+		if(val>=0 && val<26)
+			return true;
+
+		return false;
+	}
+
+	// converts value in range [0,26) to the correspondinng uppercase character
+	static char value(int val) {
+
+		// TODO check 'expected type_specifier' ans 'std' doesn't name a type
+		if(!is_valid(val)) {
+			throw new std::invalid_argument_exception { "Not a valid char value\n"
+								    "Only values in range [0, 26) are valid" };
+		}
+
+		return (val + 'A');
+	}
+
+	// TODO
+	// converts all character to uppercase
+	static void normalize(string& input) {
+		
+	}
+
+	// returns a vector of int values for the characters in 'input'
+	// the input is expected to be "normalized"
+	static vector<int> int_values(string input) {
+
+		const size_t vector_size = input.length();
+		vector<int> int_vals (vector_size);
+
+		for(char ch : input)
+			int_vals.push_back(value(ch));
+
+		return int_vals;
+	}
+
+	// creates 1*degree matrices from the given vector of integers
+	static auto int_matrices(vector<int> vals, int degree) {
+
+		// convert degree to double to trigger floating-point division
+		const size_t matrix_num = ceil(vals.size() / double(degree));
+
+		// TODO 'matrix_num' vectors of size 'degree' ?
+		vector< vector<int> > matrices (matrix_num);
+
+		for(int curr_matrix_index=0; curr_matrix_index < matrix_num; curr_matrix_index++) {
+			for(int curr_val_index=0; curr_val_index < degree; curr_val_index++) {
+				int val_index = curr_matrix_index * degree + curr_val_index;
+
+				matrices[curr_matrix_index].push_back(vals[val_index]);
+			}
+		}
+
+		return matrices;
+	}
+
+	// creates a 1*degree matrices from the given plain_text
+	// all characters of plaint text are expected to be "normalized"
+	static auto plain_text_matrices(string plain_text, int degree) {
+		// get the int values of characters
+		vector<int> int_vals = int_values(plain_text);
+
+		return int_matrices(int_vals, degree);
+		
+	}
+
+	string operator* (string plain_text, const cipher_matrix& matrix) {
+		normalize(plain_text);
+
+		auto pt_matrices = plain_text_matrices(plain_text, matrix.degree);
+	}
+}
---
+++ hill_cipher/hill_cipher.h
@@ -0,0 +1,48 @@
+#include <istream>
+#include <vector>
+using std::string;
+using std::istream;
+using std::vector;
+
+namespace hill_cipher {
+
+	// Contract:
+	// 	There is a contract between the public instance
+	// methods of this class and the overloaded "input stream"
+	// operator.
+	//
+	// Any public methods of this class must be invoked only
+	// after the "successful" invocation of the input stream
+	// operator atleast once. Else, the behaviour is "undefined".
+	// 
+	class cipher_matrix{
+		vector< vector<int> > matrix;
+		int degree;
+
+		public:
+			// delete the unwanted defaults
+			cipher_matrix() = delete;
+			cipher_matrix(cipher_matrix&& cm) = delete;
+			cipher_matrix(const cipher_matrix& cm) = delete;
+			cipher_matrix& operator= (const cipher_matrix& cm) = delete;
+			cipher_matrix& operator= (cipher_matrix&& cm) = delete;
+
+			cipher_matrix(int degree) {
+				this->degree = degree;
+				// TODO how to create multiple vectors ?
+				this->matrix = vector<vector<int>> { vector<int> { degree } };
+			}
+
+			// The function which should be invoked before any other
+			// public method of this class; as specified in the contract.
+			friend istream& operator>> (istream& is, cipher_matrix& cm);
+
+			friend string operator* (string plain_text, const cipher_matrix& matrix);
+	};
+
+	// Helper methods
+	// string operator* (const cipher_matrix& matrix, const string& plain_text);
+
+	// Wrapper for the overloaded * operator which accepts the same parameters
+	string encrypt(const cipher_matrix& matrix, const string& plaint_text);
+}
