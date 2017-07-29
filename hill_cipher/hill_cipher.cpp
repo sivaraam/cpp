78420904472d8d7c71ba9183885d0a4f973d778c
Kaartic Sivaraam <kaarticsivaraam91196@gmail.com>
2017-07-29 21:46:10 +0530

    hill_cipher: Hygiene: code cleanup
    * use empty lines where it makes 'sense'
    
    Using empty lines for consistency doesn't seem to be working
    well as it seems too spacious.
    
    Revert to using them where they make sense
    
    * use the correct datatype for unsigned values

--- hill_cipher/hill_cipher.cpp
+++ hill_cipher/hill_cipher.cpp
@@ -7,9 +7,8 @@
 namespace hill_cipher {
 
 	istream& operator>> (istream& is, cipher_matrix& cmatrix) {
-
-		for (int i=0; i<cmatrix.degree; i++) {
-			for (int j=0; j<cmatrix.degree; j++) {
+		for (size_t i=0; i<cmatrix.degree; i++) {
+			for (size_t j=0; j<cmatrix.degree; j++) {
 				is>>cmatrix.matrix[i][j];
 				/* TODO	
 				if(is::fail) {
@@ -23,57 +22,46 @@ namespace hill_cipher {
 	}
 
 	static bool is_valid(char ch) {
-
 		if(isalpha(ch) && isupper(ch))
 			return true;
 
 		return false;
-
 	}
 
 	// converts uppercase alphabet to a value iin range [0, 26)
 	static int value(char ch) {
-
 		if(!is_valid(ch))
 			throw new std::invalid_argument("Not a valid character\n"
 								  "Only uppercase alphabets are valid");
 		return (ch - 'A');
-
 	}
 
 	static bool is_valid(int val) {
-
 		if(val>=0 && val<26)
 			return true;
 
 		return false;
-
 	}
 
 	// converts value in range [0,26) to the correspondinng uppercase character
 	static char value(int val) {
-
 		if(!is_valid(val)) {
 			throw new std::invalid_argument { "Not a valid char value\n"
 								    "Only values in range [0, 26) are valid" };
 		}
 
 		return (val + 'A');
-
 	}
 
 	// converts all character to uppercase
 	static void normalize(string& input) {
-		
 		auto& facet = std::use_facet<std::ctype<char>>(std::locale());
 		facet.toupper(&input[0], &input[0] + input.length());
-
 	}
 
 	// returns a vector of int values for the characters in 'input'
 	// the input is expected to be "normalized"
 	static vector<int> int_values(string input) {
-
 		const size_t vector_size = input.length();
 		vector<int> int_vals (vector_size);
 
@@ -81,11 +69,12 @@ namespace hill_cipher {
 			int_vals.push_back(value(ch));
 
 		return int_vals;
+	}
 
 	}
 
 	// creates 1*degree matrices from the given vector of integers
-	static auto int_matrices(vector<int> vals, int degree) {
+	static auto int_matrices(vector<int> vals, unsigned degree) {
 
 		// convert degree to double to trigger floating-point division
 		const size_t matrix_num = ceil(vals.size() / double(degree));
@@ -93,31 +82,34 @@ namespace hill_cipher {
 		// 'matrix_num' vectors of size 'degree'
 		vector< vector<int> > matrices (matrix_num, vector<int>(degree));
 
-		for(int curr_matrix_index=0; curr_matrix_index < matrix_num; curr_matrix_index++) {
-			for(int curr_val_index=0; curr_val_index < degree; curr_val_index++) {
-				int val_index = curr_matrix_index * degree + curr_val_index;
 
+		for(size_t curr_matrix_index=0; curr_matrix_index < matrix_num; curr_matrix_index++) {
+
+			for(size_t curr_val_index=0; curr_val_index < degree; curr_val_index++) {
+
+				size_t val_index = curr_matrix_index * degree + curr_val_index;
 				matrices[curr_matrix_index].push_back(vals[val_index]);
+
 			}
+
 		}
 
 		return matrices;
-
 	}
 
 	// creates a 1*degree matrices from the given plain_text
 	// all characters of plaint text are expected to be "normalized"
-	static auto plain_text_matrices(string plain_text, int degree) {
+	static auto plain_text_matrices(string plain_text, unsigned degree) {
 		// get the int values of characters
 		vector<int> int_vals = int_values(plain_text);
 
 		return int_matrices(int_vals, degree);
-		
 	}
 
 	string operator* (string plain_text, const cipher_matrix& matrix) {
 		normalize(plain_text);
-
 		auto pt_matrices = plain_text_matrices(plain_text, matrix.degree);
+
+
 	}
 }
