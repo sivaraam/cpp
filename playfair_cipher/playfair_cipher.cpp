29f5a0298523e6c82b4bdd25736ae6d29ba7b4f7
Kaartic Sivaraam <kaarticsivaraam91196@gmail.com>
2017-08-02 23:23:12 +0530

    playfair_cipher: implement the core logic
    The core logic of converting plain text character pairs
    into cipher text character pairs was implemented.

--- playfair_cipher/playfair_cipher.cpp
+++ playfair_cipher/playfair_cipher.cpp
@@ -42,6 +42,50 @@ namespace playfair_cipher {
 		initialize_table(key);
 	}
 
+	pair<char, char> get_cipher_pair(playfair_table *ptable, pair<size_t, size_t> first_index, pair<size_t, size_t>second_index) {
+		auto [first_row, first_col] = first_index;
+		auto [first_cipher_row, first_cipher_col] = first_index;
+		auto [second_row, second_col] = second_index;
+		auto [second_cipher_row, second_cipher_col] = second_index;
+
+		if (first_row == second_row) {
+			first_cipher_col = (first_col + 1) % playfair_table::table_degree;
+			second_cipher_col = (second_col + 1) % playfair_table::table_degree;
+		}
+		else if (first_col == second_col) {
+			first_cipher_row = (first_row + 1) % playfair_table::table_degree;
+			second_cipher_row = (second_row + 1) % playfair_table::table_degree;
+		}
+		else {
+			first_cipher_col = second_col;
+			second_cipher_col = first_col;
+		}
+
+		const char first_cipher_char = ptable->table[first_cipher_row][first_cipher_col];
+		const char second_cipher_char = ptable->table[second_cipher_row][second_cipher_col];
+		return std::make_pair(first_cipher_char, second_cipher_char);
+	}
+
+	pair<size_t, size_t> playfair_table::get_index(char ch) {
+		for(size_t row=0; row<table_degree; row++) {
+			for(size_t col=0; col<table_degree; col++) {
+				if(table[row][col] == ch)
+					return std::make_pair(row, col);
+			}
+		}
+
+		// control should not reach here if the entry is valid
+		throw std::invalid_argument{"character not in table"};
+	}
+
+	pair<char, char> playfair_table::operator[] (pair<char, char> pt_pair) {
+		const auto [first_ch, second_ch] = pt_pair;
+		auto first_index = get_index(first_ch);
+		auto second_index = get_index(second_ch);
+
+		return get_cipher_pair(this, first_index, second_index);
+	}
+
 	string encrypt(string plain_text, string key) {
 		playfair_table ptable { key };
 		vector< pair<char, char> > ct_pairs;
--- playfair_cipher/playfair_cipher.h
+++ playfair_cipher/playfair_cipher.h
@@ -18,9 +18,13 @@ namespace playfair_cipher {
 		// the state of this object is not defined after
 		// the invocation of initialize_table
 		static string alphabets;	
-	
+
 		// used to initalize the table's contents
 		void initialize_table(string key);
+
+		// returns the index(row, col) of the given character
+		// until ch is a valid matrix entry else throws an exception
+		pair<size_t, size_t> get_index(char ch);
 
 		public:
 			// delete the unnecessary defaults
@@ -31,6 +35,10 @@ namespace playfair_cipher {
 			playfair_table& operator= (playfair_table&&) = delete;
 
 			playfair_table(const string key);
+			pair <char, char> operator[] (pair<char, char> pt_pair);
+			
+			friend pair<char, char> get_cipher_pair(playfair_table *ptable, pair<size_t, size_t> first_index, pair<size_t, size_t>second_index);
+
 	};
 
 	string encrypt(const string plain_text, const string key);
